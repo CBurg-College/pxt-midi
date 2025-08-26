617d1c4ebaacfee8de9d2c1873476a7a6a6c6ee1
CBurg-College <cburgmicro@outlook.com>
2025-08-26 22:22:41 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,41 @@
+    // 0: stop playing
+    // 1: start playing
+    // 2: pause playing
+
+    // 100-500: transposing = value - 300
+
+    // 1000-1199: instrument partiture 1
+    // 1200-1399: instrument partiture 2
+    // 1400-1599: instrument partiture 3
+    // 1600-1799: instrument partiture 4
+    // 1800-1999: instrument partiture 5
+
+    // 2000-2199: volume partiture 1
+    // 2200-2399: volume partiture 2
+    // 2400-2599: volume partiture 3
+    // 2600-2799: volume partiture 4
+    // 2800-2999: volume partiture 5
+
+    // > 3000: duration = value - 3000
+
+let MEASURE = 0
+
+type measurehandler = (measure: number) => void
+let measureHandler: measurehandler
+
+radio.onReceivedNumber(function (cmd: number) {
+    if ((cmd < 0) && measureHandler) measureHandler(-cmd)
+})
+
+//% color="#00CC00" icon="\uf1f9"
+//% block="Midi"
+//% block.loc.nl="Midi"
+namespace CMidi {
+
+    //% block="when measure %measure starts"
+    //% block.loc.nl="wanneer maat %measure begint"
+    function onMeasure(measure: number, programmableCode: (measure: number) => void): void {
+        measureHandler = programmableCode;
+    }
+
+}
