e2e3ceedf5cd14781331bc0d78b8c73170284885
CBurg-College <cburgmicro@outlook.com>
2025-08-27 21:39:35 +0200

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -20,12 +20,80 @@
 
 let MEASURE = 0
 
-let measureHandler: handler
+enum Move {
+    Move1,
+    Move2,
+    Move3,
+    Move4,
+    Move5,
+    Move6,
+    Move7,
+    Move8,
+    Move9,
+    Move10,
+    Move11,
+    Move12,
+    Move13,
+    Move14,
+    Move15,
+    Move16,
+    Move17,
+    Move18,
+    Move19,
+    Move20,
+}
+
+let doMove1: handler
+let doMove2: handler
+let doMove3: handler
+let doMove4: handler
+let doMove5: handler
+let doMove6: handler
+let doMove7: handler
+let doMove8: handler
+let doMove9: handler
+let doMove10: handler
+let doMove11: handler
+let doMove12: handler
+let doMove13: handler
+let doMove14: handler
+let doMove15: handler
+let doMove16: handler
+let doMove17: handler
+let doMove18: handler
+let doMove19: handler
+let doMove20: handler
+
+let moves: number[][] = []
 
 radio.onReceivedNumber(function (cmd: number) {
-    if (cmd < 0) {
-        MEASURE = -cmd
-        if (measureHandler) measureHandler()
+    if (cmd >= 0) return;
+    MEASURE = -cmd
+    for (let i = 0; i < moves.length; i++) {
+        if (moves[i][0] == MEASURE) {
+            switch(moves[i][1]) {
+                case Move.Move1: doMove1(); break;
+                case Move.Move2: doMove2(); break;
+                case Move.Move3: doMove3(); break;
+                case Move.Move4: doMove4(); break;
+                case Move.Move5: doMove5(); break;
+                case Move.Move6: doMove6(); break;
+                case Move.Move7: doMove7(); break;
+                case Move.Move8: doMove8(); break;
+                case Move.Move9: doMove9(); break;
+                case Move.Move10: doMove10(); break;
+                case Move.Move11: doMove11(); break;
+                case Move.Move12: doMove12(); break;
+                case Move.Move13: doMove13(); break;
+                case Move.Move14: doMove14(); break;
+                case Move.Move15: doMove15(); break;
+                case Move.Move16: doMove16(); break;
+                case Move.Move17: doMove17(); break;
+                case Move.Move18: doMove18(); break;
+                case Move.Move19: doMove19(); break;
+                case Move.Move20: doMove20(); break;
+            }
+        }
     }
 })
 
@@ -40,10 +108,36 @@ namespace CMidi {
         return MEASURE
     }
 
-    //% block="when a new measure starts"
-    //% block.loc.nl="wanneer een volgende maat begint"
-    export function onMeasure(programmableCode: () => void): void {
-        measureHandler = programmableCode;
+    //% block="do for move %move"
+    //% block.loc.nl="doe voor move %move"
+    export function onMove(move: number, programmableCode: () => void): void {
+        switch (move) {
+            case 1: doMove1 = programmableCode; break;
+            case 2: doMove2 = programmableCode; break;
+            case 3: doMove3 = programmableCode; break;
+            case 4: doMove4 = programmableCode; break;
+            case 5: doMove5 = programmableCode; break;
+            case 6: doMove6 = programmableCode; break;
+            case 7: doMove7 = programmableCode; break;
+            case 8: doMove8 = programmableCode; break;
+            case 9: doMove9 = programmableCode; break;
+            case 10: doMove10 = programmableCode; break;
+            case 11: doMove11 = programmableCode; break;
+            case 12: doMove12 = programmableCode; break;
+            case 13: doMove13 = programmableCode; break;
+            case 14: doMove14 = programmableCode; break;
+            case 15: doMove15 = programmableCode; break;
+            case 16: doMove16 = programmableCode; break;
+            case 17: doMove17 = programmableCode; break;
+            case 18: doMove18 = programmableCode; break;
+            case 19: doMove19 = programmableCode; break;
+            case 20: doMove20 = programmableCode; break;
+        }
     }
 
+    //% block="perform at measure %measure %move"
+    //% block.loc.nl="voer op maat %measure %move uit"
+    export function measureMove(measure: number, move: Move) {
+        moves.push([measure,move])
+    }
 }
